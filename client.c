89d24911828df95c0793d1a088f8f3eece8a779a
KaziKoIzak <izak.benitezlopez@jacks.sdstate.edu>
2023-09-25 20:15:59 -0500

    Modified

--- client.c
+++ client.c
@@ -1,42 +1,69 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <unistd.h>
+/****************************************************
+*
+*    Basic minimal socket client program for use
+*    in CSc 487 final projects.  You will have to
+*    enhance this for your projects!!
+*
+*                                  RSF    11/14/20
+*
+****************************************************/
+#include<stdio.h>      // used printf/scanf for demo (puts/getchar would be leaner)
+#include<string.h>	
+#include<sys/socket.h>
+#include<arpa/inet.h>  // for inet_addr and sockaddr_in structs
 
+int main(int argc , char *argv[])
+{
+	int socket_desc;    // file descripter returned by socket command
+	int read_size;
+	struct sockaddr_in server;    // in arpa/inet.h
+	char  server_reply[100], client_message[100];   // will need to be bigger
+	
+	//Create socket
+	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
 
-int main() {
-    // Create a socket
-    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_socket == -1) {
-        perror("Error creating socket");
-        exit(EXIT_FAILURE);
-    }
+	printf("Trying to create socket\n");
+	if (socket_desc == -1)
+	{
+		printf("Unable to create socket\n");
+	}
+		
+// *********** This is the line you need to edit ****************
+	server.sin_addr.s_addr = inet_addr("0.0.0.0");  // doesn't like localhost?
+	server.sin_family = AF_INET;
+	server.sin_port = htons( 8421 );    // random "high"  port number
 
-    // Define server address
-    struct sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(8080);  // Port number
-    server_addr.sin_addr.s_addr = inet_addr("0.0.0.0"); // Server IP address
+	//Connect to remote server
+	if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0)
+	{
+		printf(" connect error");
+		return 1;
+	}
+	
 
-    // Connect to server
-    if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-        perror("Error connecting");
-        close(client_socket);
-        exit(EXIT_FAILURE);
-    }
+	//Get data from keyboard and send  to server
+	printf("What do you want to send to the server. (b for bye)\n");
+	while(strncmp(client_message,"b",1))      // quit on "b" for "bye"
+	{
+		memset(client_message,'\0',100);
+		scanf("%s",client_message);
 
-    // Communication
-    const char *message = "Hello, server!";
-    send(client_socket, message, strlen(message), 0);
+		if( send(socket_desc , &client_message, strlen(client_message) , 0) < 0)
+		{
+			printf("Send failed");
+			return 1;
+		}
 
-    char buffer[1024];
-    recv(client_socket, buffer, sizeof(buffer), 0);
-    printf("Received: %s\n", buffer);
+		printf("\nSending Message: %.*s\n", (int) strlen(client_message),client_message);
+	
+		//Receive a reply from the server
+		if( (read_size = recv(socket_desc, server_reply , 100 , 0)) < 0)
+		{
+			printf("recv failed");
+		}
+		printf("Server  Replies: %.*s\n\n", read_size,server_reply);
+	}
 
-    // Clean up
-    close(client_socket);
-
-    return 0;
+	
+	return 0;
 }
